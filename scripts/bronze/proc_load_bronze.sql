2ed7988724ff674ac392cfd14381337bfe5cdc23
Công Nguyễn <ntkenc3005@gmail.com>
2026-06-23 17:49:56 +0700

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -9,69 +9,157 @@ Script Purpose
       - Uses the ' BULK INSERT; command to load data from csv files to bronze tables 
 =========================================================================
 */
+Create or alter Procedure bronze.load_bronze AS 
+Begin
+	DECLARE @start_time Datetime, @end_time Datetime
+	Begin try 
+	print '====================='
+	print 'Loading Bronze Layer'
+	print '====================='
 
-BULK INSERT bronze.account 
-		From 'E:\Data_Analysis\CzechBankProject\datasets\account.csv'
-		WITH (
-			FIRSTROW = 2 ,
-			FIELDTERMINATOR  = ';',
-			TABLOCK
-		)
+	set @start_time = getdate();
+	print 'Truncating Table : bronze.account'
+	Truncate table bronze.account;
 
-BULK INSERT bronze.card
-		From 'E:\Data_Analysis\CzechBankProject\datasets\card.csv'
-		WITH (
-			FIRSTROW = 2 ,
-			FIELDTERMINATOR  = ';',
-			TABLOCK
-		)
+	print 'Loading Table : bronze.account'
+	BULK INSERT bronze.account 
+			From 'E:\Data_Analysis\CzechBankProject\datasets\account.csv'
+			WITH (
+				FIRSTROW = 2 ,
+				FIELDTERMINATOR  = ';',
+				TABLOCK
+			)
+	set @end_time = getdate() 
+	Print '>> Load Duration : ' + cast(datediff(second,@start_time, @end_time) as nvarchar) + 'seconds' 
+	Print '                    '
+	Print '>>------------------'
 
-BULK INSERT bronze.client 
-		From 'E:\Data_Analysis\CzechBankProject\datasets\client.csv'
-		WITH (
-			FIRSTROW = 2 ,
-			FIELDTERMINATOR  = ';',
-			TABLOCK
-		)
+	set @start_time = getdate();
+	print 'Truncating Table : bronze.card'
+	Truncate table bronze.card;
 
-BULK INSERT bronze.disp 
-		From 'E:\Data_Analysis\CzechBankProject\datasets\disp.csv'
-		WITH (
-			FIRSTROW = 2 ,
-			FIELDTERMINATOR  = ';',
-			TABLOCK
-		)
+	print 'Loading Table : bronze.card'
+	BULK INSERT bronze.card
+			From 'E:\Data_Analysis\CzechBankProject\datasets\card.csv'
+			WITH (
+				FIRSTROW = 2 ,
+				FIELDTERMINATOR  = ';',
+				TABLOCK
+			)
+	set @end_time = getdate() 
+	Print '>> Load Duration : ' + cast(datediff(second,@start_time, @end_time) as nvarchar) + 'seconds' 
+	Print '                    '
+	Print '>>------------------'
+	
+	set @start_time = getdate();
+	print 'Truncating Table : bronze.client'
+	Truncate table bronze.client;
 
-Truncate table bronze.district;
-BULK INSERT bronze.district 
-		From 'E:\Data_Analysis\CzechBankProject\datasets\district.csv'
-		WITH (
-			FIRSTROW = 2 ,
-			FIELDTERMINATOR  = ';',
-			TABLOCK
-		)
+	print 'Loading Table : bronze.client'
+	BULK INSERT bronze.client 
+			From 'E:\Data_Analysis\CzechBankProject\datasets\client.csv'
+			WITH (
+				FIRSTROW = 2 ,
+				FIELDTERMINATOR  = ';',
+				TABLOCK
+			)
+	set @end_time = getdate() 
+	Print '>> Load Duration : ' + cast(datediff(second,@start_time, @end_time) as nvarchar) + 'seconds' 
+	Print '                    '
+	Print '>>------------------'
+	
+	set @start_time = getdate();
+	print 'Truncating Table : bronze.disp'
+	Truncate table bronze.disp;
 
+	print 'Loading Table : bronze.disp'
+	BULK INSERT bronze.disp 
+			From 'E:\Data_Analysis\CzechBankProject\datasets\disp.csv'
+			WITH (
+				FIRSTROW = 2 ,
+				FIELDTERMINATOR  = ';',
+				TABLOCK
+			)
+	set @end_time = getdate() 
+	Print '>> Load Duration : ' + cast(datediff(second,@start_time, @end_time) as nvarchar) + 'seconds' 
+	Print '                    '
+	Print '>>------------------'
+	
+	set @start_time = getdate();
+	print 'Truncating Table : bronze.district'
+	Truncate table bronze.district;
 
-BULK INSERT bronze.loan 
-		From 'E:\Data_Analysis\CzechBankProject\datasets\loan.csv'
-		WITH (
-			FIRSTROW = 2 ,
-			FIELDTERMINATOR  = ';',
-			TABLOCK
-		)
+	print 'Loading Table : bronze.district'
+	BULK INSERT bronze.district 
+			From 'E:\Data_Analysis\CzechBankProject\datasets\district.csv'
+			WITH (
+				FIRSTROW = 2 ,
+				FIELDTERMINATOR  = ';',
+				TABLOCK
+			)
+	set @end_time = getdate() 
+	Print '>> Load Duration : ' + cast(datediff(second,@start_time, @end_time) as nvarchar) + 'seconds' 
+	Print '                    '
+	Print '>>------------------'
 
-BULK INSERT bronze.orders 
-		From 'E:\Data_Analysis\CzechBankProject\datasets\orders.csv'
-		WITH (
-			FIRSTROW = 2 ,
-			FIELDTERMINATOR  = ';',
-			TABLOCK
-		)
+	set @start_time = getdate();
+	print 'Truncating Table : bronze.loan'
+	Truncate table bronze.loan;
 
-BULK INSERT bronze.trans 
-		From 'E:\Data_Analysis\CzechBankProject\datasets\trans.csv'
-		WITH (
-			FIRSTROW = 2 ,
-			FIELDTERMINATOR  = ';',
-			TABLOCK
-		)
+	print 'Loading Table : bronze.loan'
+	BULK INSERT bronze.loan 
+			From 'E:\Data_Analysis\CzechBankProject\datasets\loan.csv'
+			WITH (
+				FIRSTROW = 2 ,
+				FIELDTERMINATOR  = ';',
+				TABLOCK
+			)
+	set @end_time = getdate() 
+	Print '>> Load Duration : ' + cast(datediff(second,@start_time, @end_time) as nvarchar) + 'seconds' 
+	Print '                    '
+	Print '>>------------------'
+
+	set @start_time = getdate();
+	print 'Truncating Table : bronze.orders'
+	Truncate table bronze.orders;
+
+	print 'Loading Table : bronze.orders'
+	BULK INSERT bronze.orders 
+			From 'E:\Data_Analysis\CzechBankProject\datasets\orders.csv'
+			WITH (
+				FIRSTROW = 2 ,
+				FIELDTERMINATOR  = ';',
+				TABLOCK
+			)
+	set @end_time = getdate() 
+	Print '>> Load Duration : ' + cast(datediff(second,@start_time, @end_time) as nvarchar) + 'seconds' 
+	Print '                    '
+	Print '>>------------------'
+
+	set @start_time = getdate();
+	print 'Truncating Table : bronze.trans'
+	Truncate table bronze.trans;
+
+	print 'Loading Table : bronze.trans'
+	BULK INSERT bronze.trans 
+			From 'E:\Data_Analysis\CzechBankProject\datasets\trans.csv'
+			WITH (
+				FIRSTROW = 2 ,
+				FIELDTERMINATOR  = ';',
+				TABLOCK
+			)
+	set @end_time = getdate() 
+	Print '>> Load Duration : ' + cast(datediff(second,@start_time, @end_time) as nvarchar) + 'seconds' 
+	Print '                    '
+	Print '>>------------------'
+
+	End Try
+	Begin Catch 
+		Print '==============================='
+		print 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+		PRINT 'Error Message' + ERROR_Message();
+		print 'Error Message' + CAST (ERROR_NUMBER() as nvarchar);
+		print 'Error Message' + CAST (ERROR_NUMBER() as nvarchar);
+		print '==============================='
+	END CATCH
+END
