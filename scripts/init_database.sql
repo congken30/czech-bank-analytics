18feef65e95e46c61fbbc16bf95450e847fbabbe
Công Nguyễn <ntkenc3005@gmail.com>
2026-07-08 12:52:07 +0700

    Update init_database.sql script purpose description
    Removed mention of schema creation in the script purpose.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -4,8 +4,7 @@ Create Database and Schemas
 =============================================================
 Script Purpose:
     This script creates a new database named 'CzechBankDB' after checking if it already exists. 
-    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
-    within the database: 'bronze', 'silver', and 'gold'.
+    If the database exists, it is dropped and recreated. 
 	
 WARNING:
     Running this script will drop the entire 'CzechBankDB' database if it exists. 
